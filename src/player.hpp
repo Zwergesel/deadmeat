c7f9e37432b892bfa3a2a1f73f779cf319f0c148
alexwirth.mail@gmail.com <alexwirth.mail@gmail.com@92bcd401-5a1b-3187-595b-c968c4fc1f98>
2012-05-27 22:27:59 +0000

    -Added basic game time support. -Added 2 basic creatures. -Added basic game message support

--- src/player.hpp
+++ src/player.hpp
@@ -2,16 +2,21 @@
 #define _PLAYER_HPP
 
 #include <string>
+#include <libtcod.hpp>
 
 class Player
 {
 public:
-	Player(std::string name);
-	std::string name;
-	char symbol;
-	int x,y;
 	static int dx[9];
 	static int dy[9];
+
+	int x,y;
+	std::string name;
+	int symbol;
+	int nextAction;
+
+	Player(std::string name);
+	void draw(int offsetX, int offsetY);
 };
 
 #endif
