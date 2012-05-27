ec1ea5485264f9968fd8823a3ff2d040958f43fa
alexwirth.mail@gmail.com <alexwirth.mail@gmail.com@92bcd401-5a1b-3187-595b-c968c4fc1f98>
2012-05-27 12:15:11 +0000

    Fixed last revision

---
+++ src/tileset.cpp
@@ -0,0 +1,26 @@
+#include "tileset.hpp"
+
+TileSet::TileSet()
+{
+	info = new TileInfo[TILESET_LENGTH];
+	
+	// TODO: Hard-coded for now, load from file later
+	// Parameters: passable (bool), symbol (int / char), foregroundcolor, backgroundcolor
+	info[TILE_CAVE_FLOOR]	= TileInfo(true, '.', TCODColor::grey, TCODColor::white);
+	info[TILE_CAVE_WALL]	= TileInfo(false, TCOD_CHAR_BLOCK1, TCODColor::grey, TCODColor(125,95,64));
+	info[TILE_STONE_FLOOR]	= TileInfo(true, '.', TCODColor::grey, TCODColor::white);
+	info[TILE_MEADOW]		= TileInfo(true, '.', TCODColor::green, TCODColor::white);
+}
+
+TileSet::~TileSet()
+{
+	if (info != NULL) {
+		delete[] info;
+		info = NULL;
+	}
+}
+
+void TileSet::setTile(Tile t, TileInfo inf)
+{
+	info[t] = inf;
+}
