edfb3c82487803b1839f8646c559f8da0b97fc72
alexwirth.mail@gmail.com <alexwirth.mail@gmail.com@92bcd401-5a1b-3187-595b-c968c4fc1f98>
2012-05-27 03:12:08 +0000

    -Improved TileSet support (map should not store redundant information for each tile) -Removed setSize from Level class (should never be necessary)

--- src/level.cpp
+++ src/level.cpp
@@ -8,16 +8,15 @@ Level::Level(int width, int height)
 	this->width = width;
 	this->height = height;
 	this->map = new Tile[width*height];
-	std::fill(map, map+width*height, Tile('.', true));
+	std::fill(map, map+width*height, TILE_CAVE_FLOOR);
 }
 
 Level::~Level() 
 {
-	if (map != NULL)
-  {
-    delete[] map;
-    map = NULL;
-  }
+	if (map != NULL) {
+		delete[] map;
+		map = NULL;
+	}
 }
 
 inline int Level::coord(int x, int y) 
@@ -27,43 +26,36 @@ inline int Level::coord(int x, int y)
 
 void Level::display(int offsetX, int offsetY) 
 {	
-  int startX = (offsetX < 0) ? -offsetX : 0;
-  int startY = (offsetY < 0) ? -offsetY : 0;
-  int rangeX = std::min(width - offsetX, TCODConsole::root->getWidth());
-  int rangeY = std::min(height - offsetY, TCODConsole::root->getHeight() - 1);
-	for (int y=startY; y<rangeY; y++) 
-  {
-	  for (int x=startX; x<rangeX; x++) 
-    {
-		  //std::cerr << coord(x+offsetX,y+offsetY) << std::endl;
-      TCODConsole::root->setChar(x, y, map[coord(x+offsetX,y+offsetY)].symbol);
-	  }
+	int startX = (offsetX < 0) ? -offsetX : 0;
+	int startY = (offsetY < 0) ? -offsetY : 0;
+	int rangeX = std::min(width - offsetX, TCODConsole::root->getWidth());
+	int rangeY = std::min(height - offsetY, TCODConsole::root->getHeight() - 1);
+	for (int y=startY; y<rangeY; y++) {
+		for (int x=startX; x<rangeX; x++) {
+			// TODO: put globalTileSet into a World class and only keep World global
+			TileInfo inf = globalTileSet.info[map[coord(x+offsetX,y+offsetY)]];
+			TCODConsole::root->putCharEx(x, y, inf.symbol, inf.color, inf.background);
+		}
 	}
 }
 
 void Level::setTile(int x, int y, Tile t)
 {
-  if(map == NULL || x < 0 || y < 0 || x >= width || y >= height) return;
-  map[coord(x, y)] = t;
+	if (map == NULL || x < 0 || y < 0 || x >= width || y >= height) return;
+	map[coord(x, y)] = t;
 }
 
 Tile Level::getTile(int x, int y)
 {
-  return map[coord(x, y)];
-}
-
-void Level::setSize(int width, int height)
-{
-  this->width = width;
-  this->height = height;
+	return map[coord(x, y)];
 }
 
 int Level::getWidth()
 {
-  return width;
+	return width;
 }
 
 int Level::getHeight()
 {
-  return height;
+	return height;
 }
