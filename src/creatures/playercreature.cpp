695a27720e9f2a4f346900591c8f8454a04eda55
alexwirth.mail@gmail.com <alexwirth.mail@gmail.com@92bcd401-5a1b-3187-595b-c968c4fc1f98>
2012-09-21 16:43:23 +0000

    -Poison -PlayerCreature::save/load

--- src/creatures/playercreature.cpp
+++ src/creatures/playercreature.cpp
@@ -1,5 +1,11 @@
-#include "creature.hpp"
-#include "chargen.hpp"
+#include "../creature.hpp"
+#include "../chargen.hpp"
+#include "../savegame.hpp"
+
+PlayerCreature::PlayerCreature()
+{
+	// for savegames
+}
 
 PlayerCreature::PlayerCreature(PlayerClass c, PlayerRace r, Gender g) : 
   Creature(CharGen::RACE_NAMES[r], F_DEFAULT, (unsigned char)'@', TCODColor::black, 200, 75, 
@@ -11,6 +17,14 @@ PlayerCreature::PlayerCreature(PlayerClass c, PlayerRace r, Gender g) :
 	setDefenseSkill(0);
 }
 
+void PlayerCreature::copyFrom(PlayerCreature* original)
+{
+	Creature::copyFrom(original);
+	pclass = original->pclass;
+	race = original->race;
+	gender = original->gender;
+}
+
 void PlayerCreature::affect(Status type, int start, int duration, int strength)
 {
 	if (race == RACE_UNDEAD)
@@ -19,4 +33,35 @@ void PlayerCreature::affect(Status type, int start, int duration, int strength)
 		if (type == STATUS_POISON) return;
 	}
 	Creature::affect(type, start, duration, strength);
+}
+
+/*--------------------- SAVING AND LOADING ---------------------*/
+
+void PlayerCreature::storeAll(Savegame& sg, SaveBlock& store)
+{
+	Creature::storeAll(sg, store);
+	store ("class", (int) pclass) ("race", (int) race) ("gender", (int) gender);
+}
+
+unsigned int PlayerCreature::save(Savegame& sg)
+{
+	unsigned int id;
+	if (sg.saved(this, &id)) return id;
+	SaveBlock store("PlayerCreature", id);
+	storeAll(sg, store);
+	sg << store;
+	return id;
+}
+
+void PlayerCreature::load(LoadBlock& load)
+{
+	Creature::load(load);
+	int c, r, g;
+	load ("class", c) ("race", r) ("gender", g);
+	if (c < 0 || c >= NUM_CLASS) throw SavegameFormatException("Playercreature::load _ class out of bounds");
+	if (r < 0 || r >= NUM_RACE) throw SavegameFormatException("Playercreature::load _ race out of bounds");
+	if (g < 0 || g >= NUM_GENDER) throw SavegameFormatException("Playercreature::load _ gender out of bounds");
+	pclass = static_cast<PlayerClass>(c);
+	race = static_cast<PlayerRace>(r);
+	gender = static_cast<Gender>(g);
 }
