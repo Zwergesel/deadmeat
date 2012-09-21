1680e76da53654da733378eefc8866dd483b0115
alexwirth.mail@gmail.com <alexwirth.mail@gmail.com@92bcd401-5a1b-3187-595b-c968c4fc1f98>
2012-09-21 16:01:34 +0000

    -PlayerCreature

--- trunk/src/creatures/playercreature.cpp
+++ trunk/src/creatures/playercreature.cpp
@@ -3,9 +3,20 @@
 
 PlayerCreature::PlayerCreature(PlayerClass c, PlayerRace r, Gender g) : 
   Creature(CharGen::RACE_NAMES[r], F_DEFAULT, (unsigned char)'@', TCODColor::black, 200, 75, 
-    Weapon("fists", F_PLURAL, '#', TCODColor::pink, 1, 0, 8, 0, 3, 1, 2, 0, 2, EFFECT_NONE, 1, AMMO_NONE, -1), 0, 10, 0, "")
+    Weapon("fists", F_PLURAL, '#', TCODColor::pink, 1, 0, 8, 0, 3, 1, 2, 0, 2, EFFECT_NONE, 1, AMMO_NONE, -1), 0, 10, 0, ""),
+	pclass(c), race(r), gender(g)
 {	
 	setControlled(true);
 	setAttackSkill(0);
 	setDefenseSkill(0);
+}
+
+void PlayerCreature::affect(Status type, int start, int duration, int strength)
+{
+	if (pclass == CLASS_UNDEAD)
+	{
+		// Poison immunity
+		if (type == STATUS_POISON) return;
+	}
+	Creature::affect(type, start, duration, strength);
 }
